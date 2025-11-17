ac716e7752a64beae2f7b4c8180d77c1b6c85309
wilson <wilson.lima.silva@icloud.com>
2025-11-17 15:54:57 -0300

    Criado projeto com .net 9 para mostrar o uso e configuração do openai/v1.json

---
+++ API_Net9/Program.cs
@@ -0,0 +1,23 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi(); //se não informar nada o nome padrão é //v1.json
+//builder.Services.AddOpenApi("v2"); //nomeando para v2.json 
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
