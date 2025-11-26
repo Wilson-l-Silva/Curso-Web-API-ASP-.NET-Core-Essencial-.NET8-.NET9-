b0958e97d4c0f9d1ba970f8640c776e7a1e8e2b4
wilson <wilson.lima.silva@icloud.com>
2025-11-26 09:00:35 -0300

    Adicionado as entidades de dominio, Categoria e Produto

---
+++ APICatalogo/Models/Categoria.cs
@@ -0,0 +1,10 @@
+﻿namespace APICatalogo.Models; //scoped namespace, se por ; no final não precisa usar as chaves do namespace
+
+//Classes anemicas, ela só tem propriedades e não tem comportamento (Metodos)
+
+public class Categoria
+{
+    public int CategoriaId { get; set; }
+    public string? Nome { get; set; }
+    public string? ImagemUrl { get; set; }
+}
---
+++ APICatalogo/Models/Produto.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace APICatalogo.Models;
+
+//Classes anemicas, ela só tem propriedades e não tem comportamento (Metodos)
+public class Produto
+{
+    public int ProdutoId { get; set; }
+    public string? Nome { get; set; }
+    public string? Descricao { get; set; }
+    public decimal Preco { get; set; }
+    public string? ImagemUrl { get; set; }
+    public float Estoque { get; set; }
+    public DateTime DataCadastro { get; set; }
+
+}
