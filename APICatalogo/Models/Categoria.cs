1d49ee07d05fc4b63f4161e19a11088547b12344
wilson <wilson.lima.silva@icloud.com>
2025-11-28 09:08:21 -0300

    Adicionado relacionamento uma Categoria para muitos Produtos usando o EF Core.

--- APICatalogo/Models/Categoria.cs
+++ APICatalogo/Models/Categoria.cs
@@ -1,10 +1,18 @@
-﻿namespace APICatalogo.Models; //scoped namespace, se por ; no final não precisa usar as chaves do namespace
+﻿using System.Collections.ObjectModel;
+
+namespace APICatalogo.Models; //scoped namespace, se por ; no final não precisa usar as chaves do namespace
 
 //Classes anemicas, ela só tem propriedades e não tem comportamento (Metodos)
 
 public class Categoria
 {
+    public Categoria()
+    {
+        Produtos = new Collection<Produto>();
+    }
     public int CategoriaId { get; set; }
     public string? Nome { get; set; }
     public string? ImagemUrl { get; set; }
+
+    public ICollection<Produto>? Produtos { get; set; }
 }
